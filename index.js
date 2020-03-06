15b57c3a2e07e694b9e7e980e40c2d788ca87207
manchandajayant <maanjayant@gmail.com>
2020-03-06 10:37:04 +0100

    post endpoint and database added

--- index.js
+++ index.js
@@ -2,4 +2,19 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
-app.listen(port, () => console.log(`This app is on port ${port}`));
+const db = {}; //fake database
+
+db.messages = []; //fake table
+
+const parser = express.json(); //parser for inbuilt parser function,parsing the text to the body
+app.use(parser); //using the parser function
+
+app.post("/message", (req, res) => {
+  const { text } = req.body; //destrcuture text
+  db.messages.push(text); //push it into the fake databse  array
+  res.send(text); //response as text
+
+  console.log("this is the database", db); //console.log the database
+});
+
+app.listen(port, () => console.log(`This app is on port ${port}`)); // app running on port, logged
