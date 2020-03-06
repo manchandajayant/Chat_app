0a2755f9412bc8e48ca928e7176909188560d6b2
manchandajayant <maanjayant@gmail.com>
2020-03-06 11:02:00 +0100

    server streaming functioning

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+const Sse = require("json-sse");
 
 const db = {}; //fake database
 
@@ -9,9 +10,18 @@ db.messages = []; //fake table
 const parser = express.json(); //parser for inbuilt parser function,parsing the text to the body
 app.use(parser); //using the parser function
 
+const stream = new Sse(); //initiate a stream inside a stream
+
+app.get("/stream", (req, res) => {
+  //connecting to the stream endpoint
+  stream.updateInit(db.messages);
+  stream.init(req, res);
+});
+
 app.post("/message", (req, res) => {
   const { text } = req.body; //destrcuture text
   db.messages.push(text); //push it into the fake databse  array
+  stream.send(text); //sending the data on the stream
   res.send(text); //response as text
 
   console.log("this is the database", db); //console.log the database
